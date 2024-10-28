f5f2f3510919aa1eaa64b6d0979d55ff9f037071
srx <srx92@protonmail.com>
2024-10-28 15:36:12 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,38 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+class TestWordForIsogram:
+    @pytest.mark.parametrize(
+        "word,expected_result",
+        [
+            pytest.param(
+                "",
+                True,
+                id="empty string should return True"
+            ),
+            pytest.param(
+                "word",
+                True,
+                id="should return True if every letter is single in `word`"
+            ),
+            pytest.param(
+                "coMmit",
+                False,
+                id="should return False if `word` has the same letter in "
+                   "lower and upper cases"
+            ),
+            pytest.param(
+                "Electricity",
+                False,
+                id="should return False if `word` has duplicated letters"
+            )
+        ]
+    )
+    def test_is_isogram_func(
+            self,
+            word: str,
+            expected_result: bool
+    ) -> None:
+        assert is_isogram(word) == expected_result
